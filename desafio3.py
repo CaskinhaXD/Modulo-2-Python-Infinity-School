96618403463e075e76f5ab8849f6bd8e3a04ae84
Bernardo de Castro <69211132+Se7enzito@users.noreply.github.com>
2023-08-11 20:54:46 -0300

    Update desafio3.py

--- desafio3.py
+++ desafio3.py
@@ -11,18 +11,20 @@
 #     return logica_codigo(n - 1)
 # print(logica_codigo(10))
 
-def lista_numeros(lista: list):
-    if (len(lista) == 0):
+def lista_numeros(max: int, lista: list, count: int):
+    if (len(lista) == count):
         return
 
-    n = max(lista)
+    n = max
 
-    print(n)
+    if (n in lista):
+        print(n)
+        count = count + 1
 
-    lista.remove(n)
-
-    return lista_numeros(lista)
+    return lista_numeros(n - 1, lista, count)
 
 lista = [1, 3, 4, 5, 6, 7]
 
-lista_numeros(lista)
+count = 0
+
+lista_numeros(max(lista), lista, count)
